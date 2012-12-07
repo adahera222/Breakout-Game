fce29772e6d34131efef3398f125e487e4658402
Kasper Lind Sørensen <omglolkkthxbye@gmail.com>
2012-12-07 21:08:06 +0100

    Changed paddle collision to change the balls direction based on its distance from the centre of the paddle. Added bricks. Bricks change the balls direction based on top / bottom and right / left side collisions.

---
+++ Keep Up/Assets/MyScripts/CollisionDetection/BrickCollision.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+
+public class BrickCollision : MonoBehaviour {
+	private Vector3 bounce = new Vector3(0,0,0);
+	
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "Ball")
+		{
+			BallMovement ballMovement = collision.gameObject.GetComponent<BallMovement>();
+			bounce = ballMovement.Direction;
+			
+			// If collision hit the top of the boxcollider
+//			if (collision.contacts[0].normal == gameObject.transform.up)
+//				Debug.Log("Hit the bottom of the boxcollider");
+//			else if (collision.contacts[0].normal == -gameObject.transform.up)
+//				Debug.Log("Hit the top of the boxcollider");
+//			else if (collision.contacts[0].normal == gameObject.transform.right)
+//				Debug.Log("Hit the left side of the boxcollider");
+//			else if (collision.contacts[0].normal == -gameObject.transform.right)
+//				Debug.Log("Hit the right side of the boxcollider");
+			
+			if (collision.contacts[0].normal == gameObject.transform.up || collision.contacts[0].normal == -gameObject.transform.up)
+			{
+				Debug.Log("Hit top or bottom");
+				bounce.y = -bounce.y;
+			}
+			else if (collision.contacts[0].normal == gameObject.transform.right || collision.contacts[0].normal == -gameObject.transform.right)
+			{
+				Debug.Log("Hit right or left side");
+				bounce.x = -bounce.x;
+			}
+			ballMovement.Direction = bounce;
+			Destroy(gameObject);
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Keep Up/Assets/MyScripts/CollisionDetection/PaddleCollisionDetector.cs
+++ Keep Up/Assets/MyScripts/CollisionDetection/PaddleCollisionDetector.cs
@@ -6,7 +6,9 @@ public class PaddleCollisionDetector : MonoBehaviour {
 	public Vector3 bounceRight = new Vector3(1,1,0);
 	public Vector3 bounceLeft = new Vector3(-1,1,0);
 	public Vector3 bounceCenter = new Vector3(0,1,0);
-	
+	public Vector3 bounce = new Vector3(0,1,0);
+	float distance;
+
 	void OnCollisionEnter (Collision collision)
 	{
 		// Collision.GameObject = Ball
@@ -15,21 +17,32 @@ public class PaddleCollisionDetector : MonoBehaviour {
 		if (collision.gameObject.tag == "Ball")
 		{
 			BallMovement ballMovement = collision.gameObject.GetComponent<BallMovement>();
-			if (collision.contacts[0].point.x > gameObject.transform.position.x)
+			//bounce = ballMovement.Direction;
+			if (collision.contacts[0].point.x != gameObject.transform.position.x)
 			{
-				Debug.Log("Hit Right Side Of The Paddle");
-				ballMovement.Direction = bounceRight;
+				bounce.x = collision.contacts[0].point.x - gameObject.transform.position.x;
 				
+				ballMovement.Direction = bounce;
 			}
-			else if (collision.contacts[0].point.x < gameObject.transform.position.x)
-			{
-				Debug.Log("Hit Left Side Of The Paddle");
-				ballMovement.Direction = bounceLeft;
-			}
+			
+//			if (collision.contacts[0].point.x > gameObject.transform.position.x)
+//			{
+//				//Debug.Log("Hit Right Side Of The Paddle");
+//				distance = collision.contacts[0].point.x - gameObject.transform.position.x;
+//				bounceRight.x = distance;
+//				ballMovement.Direction = bounceRight;
+//				
+//			}
+//			else if (collision.contacts[0].point.x < gameObject.transform.position.x)
+//			{
+//				//Debug.Log("Hit Left Side Of The Paddle");
+//				distance = gameObject.transform.position.x - collision.contacts[0].point.x;
+//				bounceLeft.x = distance;
+//				ballMovement.Direction = bounceLeft;
+//			}
 			else if (collision.contacts[0].point.x == gameObject.transform.position.x)
 			{
 				Debug.Log("Hit Center Of The Paddle");
-				
 				ballMovement.Direction = bounceCenter;
 			}
 		}
